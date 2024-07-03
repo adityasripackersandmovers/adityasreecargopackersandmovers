52d371f423bc24096790244e2442b213b5a6f9ed
bhanuprakashsfdc <bhanusunny7@gmail.com>
2024-07-03 11:41:05 +0530

    pushing intial realses

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p> Aditya Sri Cargo Packers and Movers</p>
+    </>
+  )
+}
+
+export default App
