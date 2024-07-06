2617df2f244d7e02402a8856310aa7da33130380
bhanuprakashsfdc <bhanusunny7@gmail.com>
2024-07-06 20:26:03 +0530

    Pushing assets to public folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
